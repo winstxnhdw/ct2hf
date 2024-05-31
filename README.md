1d3992943cbb307d1616d137540500cc175d806c
winstxnhdw <winstxnhdw@gmail.com>
2024-06-01 03:19:18 +0800

    refactor/docs: populate README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # ct2hf
+
+`ct2hf` is a friendly CLI tool for converting a supported Hugging Face transformer model to its [CTranslate2](https://github.com/OpenNMT/CTranslate2)-compatible format and uploading them to your Hugging Face repository. If you're tired of manually converting models and running into low-memory or storage issues, `ct2hf` is the perfect tool for you.
+
+## Installation
+
+You can install `ct2hf` via pip with the following.
+
+```bash
+pip install git+https://github.com/winstxnhdw/ct2hf
+```
+
+You can also uninstall `ct2hf` via pip with the following.
+
+```bash
+pip uninstall ct2hf
+```
+
+## Usage
+
+By default, `ct2hf` avoids leaving behind any unnecessary files. If you would like to preserve the downloaded models, you can use the `--preserve-models` flag.
+
+```yaml
+usage: ct2hf
+
+convert and upload a transformer model to huggingface
+
+positional arguments:
+  model-id              transformer model to convert
+
+options:
+  --output-name         name of the output model
+  --files-to-copy       files to copy to the output model
+  --preserve-models     do not delete the downloaded models
+```
+
+### Example
+
+```bash
+ct2hf openchat/openchat-3.6-8b-20240522
+```
---
+++ ct2hf/model_converter.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from shutil import rmtree
+
+from ctranslate2.converters import TransformersConverter
+from huggingface_hub import HfApi, snapshot_download
+from typing_extensions import Self
+
+
+class ModelConverter:
+    __slots__ = ('preserve_models', 'snapshot_path', 'converted_model_path')
+
+    def __init__(self, model_id: str, output_name: str, preserve_models: bool, files_to_copy: list[str]):
+        converter = TransformersConverter(
+            model_id,
+            copy_files=files_to_copy,
+            load_as_float16=True,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+        )
+
+        self.preserve_models = preserve_models
+        self.snapshot_path = Path(snapshot_download(model_id)).parent.parent
+        self.converted_model_path = Path(converter.convert(
+            output_name or f"{model_id.split('/')[1]}-ct2-int8",
+            quantization='int8',
+            force=True,
+        ))
+
+
+    def __enter__(self) -> Self:
+        if not self.preserve_models:
+            rmtree(self.snapshot_path)
+
+        return self
+
+
+    def __exit__(self, *_):
+        if self.preserve_models:
+            return
+
+        rmtree(self.snapshot_path, ignore_errors=True)
+        rmtree(self.converted_model_path, ignore_errors=True)
+
+
+    def generate_gitattributes(self):
+        with open(self.converted_model_path/'.gitattributes', 'w', encoding='utf-8') as file:
+            file.writelines(
+                f'{path.relative_to(self.converted_model_path)} filter=lfs diff=lfs merge=lfs -text'
+                for path in self.converted_model_path.rglob('*')
+                if path.is_file() and path.stat().st_size > 10485760
+            )
+
+
+    def upload_to_huggingface(self, converted_model: str):
+        hf_api = HfApi()
+        repository_id = f"{hf_api.whoami()['name']}/{converted_model}"
+
+        hf_api.create_repo(repository_id, exist_ok=True)
+        hf_api.delete_file('.gitattributes', repository_id)
+        hf_api.upload_folder(
+            repo_id=repository_id,
+            folder_path=converted_model,
+            commit_message=f'feat: add {converted_model} model',
+        )
