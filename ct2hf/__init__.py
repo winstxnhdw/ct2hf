04ce42b8e5a25a1394acbc340ca6c8331739fbb2
winstxnhdw <winstxnhdw@gmail.com>
2024-11-04 01:45:45 +0000

    refactor/ci: drop `pylint`

--- ct2hf/__init__.py
+++ ct2hf/__init__.py
@@ -4,22 +4,27 @@ from ct2hf.model_converter import ModelConverter
 
 
 def parse_args() -> Namespace:
-    parser = ArgumentParser(description='convert and upload a transformer model to huggingface')
-    parser.add_argument('model_id', type=str, help='transformer model to convert')
-    parser.add_argument('--output-name', type=str, help='name of the output model')
-    parser.add_argument('--files-to-copy', type=str, nargs='+', help='files to copy to the output model', default=[])
-    parser.add_argument('--preserve-models', action='store_true', help='do not delete the downloaded models')
+    parser = ArgumentParser(description="convert and upload a transformer model to huggingface")
+    parser.add_argument("model_id", type=str, help="transformer model to convert")
+    parser.add_argument("--output-name", type=str, help="name of the output model")
+    parser.add_argument("--files-to-copy", type=str, nargs="+", help="files to copy to the output model", default=[])
+    parser.add_argument("--preserve-models", action="store_true", help="do not delete the downloaded models")
 
     return parser.parse_known_args()[0]
 
 
-def main():
+def main() -> None:
     args = parse_args()
 
-    with ModelConverter(args.model_id, args.output_name, args.preserve_models, args.files_to_copy) as converter:
+    with ModelConverter(
+        args.model_id,
+        args.output_name,
+        files_to_copy=args.files_to_copy,
+        preserve_models=args.preserve_models,
+    ) as converter:
         converter.generate_gitattributes()
         converter.upload_to_huggingface()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ct2hf/model_converter.py
+++ ct2hf/model_converter.py
@@ -7,18 +7,20 @@ from ctranslate2.converters import TransformersConverter
 from huggingface_hub import HfApi
 from huggingface_hub.constants import HF_HUB_CACHE
 from huggingface_hub.file_download import repo_folder_name
+from typing_extensions import Self
 
 
 class ModelConverter:
-    __slots__ = ('preserve_models', 'storage_path', 'output_directory', 'converted_model_path')
+    __slots__ = ("preserve_models", "storage_path", "output_directory", "converted_model_path")
 
     def __init__(
         self,
         model_id: str,
         output_name: str,
-        preserve_models: bool,
+        *,
         files_to_copy: list[str],
-    ):
+        preserve_models: bool,
+    ) -> None:
         converter = TransformersConverter(
             model_id,
             copy_files=files_to_copy,
@@ -28,32 +30,34 @@ class ModelConverter:
         )
 
         self.preserve_models = preserve_models
-        self.storage_path = Path(HF_HUB_CACHE) / repo_folder_name(repo_id=model_id, repo_type='model')
+        self.storage_path = Path(HF_HUB_CACHE) / repo_folder_name(repo_id=model_id, repo_type="model")
         self.output_directory = output_name or f"{model_id.split('/')[1]}-ct2-int8"
-        self.converted_model_path = Path(converter.convert(self.output_directory, quantization='int8'))
+        self.converted_model_path = Path(converter.convert(self.output_directory, quantization="int8"))
 
-    def __enter__(self) -> ModelConverter:
+    def __enter__(self) -> Self:
         if not self.preserve_models:
             rmtree(self.storage_path, ignore_errors=True)
 
         return self
 
-    def __exit__(self, *_):
+    def __exit__(self, *_) -> None:
         if self.preserve_models:
             return
 
         rmtree(self.storage_path, ignore_errors=True)
         rmtree(self.converted_model_path, ignore_errors=True)
 
-    def generate_gitattributes(self):
-        with open(self.converted_model_path / '.gitattributes', 'w', encoding='utf-8') as file:
+    def generate_gitattributes(self) -> None:
+        ten_megabytes = 10485760
+
+        with (self.converted_model_path / ".gitattributes").open("w", encoding="utf-8") as file:
             file.writelines(
-                f'{path.relative_to(self.converted_model_path)} filter=lfs diff=lfs merge=lfs -text'
-                for path in self.converted_model_path.rglob('*')
-                if path.is_file() and path.stat().st_size > 10485760
+                f"{path.relative_to(self.converted_model_path)} filter=lfs diff=lfs merge=lfs -text"
+                for path in self.converted_model_path.rglob("*")
+                if path.is_file() and path.stat().st_size > ten_megabytes
             )
 
-    def upload_to_huggingface(self):
+    def upload_to_huggingface(self) -> None:
         hf_api = HfApi()
         repository_id = f"{hf_api.whoami()['name']}/{self.output_directory}"
 
@@ -61,5 +65,5 @@ class ModelConverter:
         hf_api.upload_folder(
             repo_id=repository_id,
             folder_path=self.output_directory,
-            commit_message=f'feat: add {self.output_directory} model',
+            commit_message=f"feat: add {self.output_directory} model",
         )
