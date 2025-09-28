208cf7bb62b8a3c9c64fcf6123a591a20f12577d
winstxnhdw <winstxnhdw@gmail.com>
2025-09-28 11:02:50 +0800

    feat/docs: always use default `dtype`

--- ct2hf/convert.py
+++ ct2hf/convert.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from functools import partial
-from logging import NOTSET, basicConfig, getLogger
+from json import load
+from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 from shutil import rmtree
 from typing import TYPE_CHECKING, Any, Callable
@@ -9,9 +10,8 @@ from weakref import finalize
 
 from ctranslate2.converters import TransformersConverter
 from huggingface_hub import HfApi
-from huggingface_hub.constants import HF_HUB_CACHE
+from huggingface_hub.constants import DEFAULT_REVISION, HF_HUB_CACHE
 from huggingface_hub.file_download import repo_folder_name
-from torch import bfloat16
 
 if TYPE_CHECKING:
     from ct2hf.parse_args import Arguments
@@ -50,16 +50,24 @@ def generate_gitattributes(repository_directory: Path, min_lfs_size: int = 10485
     return repository_directory
 
 
-def load_model_patch(load_model: Callable[..., Any], *_, **kwargs: Any) -> Any:  # noqa: ANN401
+def load_model_patch(load_model: Callable[..., Any], model_refs_path: Path, *_, **kwargs: Any) -> Any:  # noqa: ANN401
+    with (
+        model_refs_path.open("r") as commit_hash,
+        (model_refs_path.parent.parent / "snapshots" / commit_hash.read().strip() / "config.json").open("r") as json,
+    ):
+        config = load(json)
+
     kwargs.pop("torch_dtype")
-    kwargs["dtype"] = bfloat16
+    kwargs["dtype"] = config.get("dtype") or config.get("torch_dtype") or "bfloat16"
+
     return load_model(*_, **kwargs)
 
 
 def convert(args: Arguments) -> None:
-    basicConfig(level=NOTSET, format="%(message)s")
+    basicConfig(level=INFO, format="%(message)s")
     logger = getLogger(__name__)
     storage_path = Path(HF_HUB_CACHE) / repo_folder_name(repo_id=args.model_id, repo_type="model")
+    refs_path = storage_path / "refs" / (args.revision or DEFAULT_REVISION)
     model_name = args.model_id.split("/", 1)[1]
     quantisation = args.quantisation
     output_directory = args.output_name or f"{model_name}-ct2-{quantisation}" if quantisation else f"{model_name}-ct2"
@@ -81,7 +89,7 @@ def convert(args: Arguments) -> None:
         trust_remote_code=True,
     )
 
-    converter.load_model = partial(load_model_patch, converter.load_model)
+    converter.load_model = partial(load_model_patch, converter.load_model, refs_path)
     converted_model_path = Path(converter.convert(output_directory, quantization=quantisation))
     repository_path = generate_gitattributes(converted_model_path)
     repository_url = upload_to_huggingface(repository_path)
